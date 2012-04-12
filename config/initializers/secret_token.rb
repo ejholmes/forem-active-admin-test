afa0c4362aa98cd0fd1a5c5c171a6423ac711f98
Eric J. Holmes <eric@ejholmes.net>
2012-04-11 18:26:22 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForemTest::Application.config.secret_token = '7a1982e89642d0f1732e36dc5185c0743b0e98176964cf012f26fb98aa5af6fe5738ad94fdea138bc79c705b63ed84f621b99526225416be7c77c285efa75167'
